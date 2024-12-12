f519757ba46d19d5e0b617f9d5e51fe9013e108d
kayatsa <kayatsa8@gmail.com>
2024-12-12 09:48:50 +0200

    Service: support add, get, getAll for model files

--- Server/ModelTrainer/Service.py
+++ Server/ModelTrainer/Service.py
@@ -1,19 +1,54 @@
 import os
+from typing import List
 
-FOLDER: str = "SavedModels"
+SAVED_FOLDER: str = "SavedModels"
+TRAINED_FOLDER: str = "TrainedModels"
 
 class Service:
 
-    def addModel(self, modelFile) -> None:
-        os.makedirs(FOLDER, exist_ok=True)
+    def __init__(self):
+        os.makedirs(SAVED_FOLDER, exist_ok=True)
+        os.makedirs(TRAINED_FOLDER, exist_ok=True)
 
-        filePath = os.path.join(FOLDER, modelFile.name)
+    def addModel(self, modelFile) -> None:
+        filePath = os.path.join(SAVED_FOLDER, modelFile.name)
 
         with open(filePath, "w") as f:
             f.write(modelFile.read())
 
         # TODO: change to save in something destributed
         
+    def getModel(self, modelName: str):
+        filePath = os.path.join(SAVED_FOLDER, modelName + ".py")
+
+        if not os.path.isfile(filePath):
+            raise Exception(f"the model {modelName} does not exist")
+        
+        return open(filePath, "r")
+        
+    def getModelsNames(self) -> List[str]:
+        models: List[str] = [file.split(".")[0] for file in os.listdir(SAVED_FOLDER) if os.path.isfile(os.path.join(SAVED_FOLDER, file))]
+
+        return models
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+service: Service = Service()
+
+
+
 
 
 
