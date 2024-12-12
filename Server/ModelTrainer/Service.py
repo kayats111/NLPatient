a65c54deada3c6b91a1c40f4f1c627c4b488e971
kayatsa <kayatsa8@gmail.com>
2024-12-12 08:57:42 +0200

    Service: addModel v1, saves locally.

--- Server/ModelTrainer/Service.py
+++ Server/ModelTrainer/Service.py
@@ -1,5 +1,17 @@
+import os
+
+FOLDER: str = "SavedModels"
+
 class Service:
-    
+
+    def addModel(self, modelFile) -> None:
+        os.makedirs(FOLDER, exist_ok=True)
+
+        filePath = os.path.join(FOLDER, modelFile.name)
+
+        with open(filePath, "w") as f:
+            f.write(modelFile.read())
+        
 
 
 
