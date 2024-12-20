98cb651326f942cae110ffc47efb3bc6a6b84b46
kayatsa <kayatsa8@gmail.com>
2024-12-20 11:21:05 +0200

    Server/Classifiers/Service: getClassifiersNames()

--- Server/Classifiers/Service.py
+++ Server/Classifiers/Service.py
@@ -1,4 +1,5 @@
 import os
+from typing import List
 
 
 
@@ -9,6 +10,12 @@ class Service:
 
     def __init__(self):
         os.makedirs(TRAINED_FOLDER, exist_ok=True)
+
+    def getClassifierNames(self):
+        classifiers: List[str] = [file.split(".")[0] for file in os.listdir(TRAINED_FOLDER)
+                                  if os.path.isfile(os.path.join(TRAINED_FOLDER, file))]
+
+        return classifiers
 
 
 
