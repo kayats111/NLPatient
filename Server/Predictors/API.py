a5571be006b48175690608a5efc26217475e3816
kayatsa <kayatsa8@gmail.com>
2024-12-27 14:49:57 +0200

    Server/Predictors/API: basic API blueprint

--- Server/Predictors/API.py
+++ Server/Predictors/API.py
@@ -4,3 +4,73 @@ from flask_cors import CORS
 from Response import Response
 from Service import Service
 import yaml
+
+
+with open('conf.yaml') as f:
+    conf = yaml.safe_load(f)
+
+port = conf["API"]["port"]
+
+
+
+app: Flask = Flask(__name__)
+CORS(app)
+bp = Blueprint('Predictors', __name__, url_prefix="/api/predictors")
+
+service: Service = Service()
+
+
+
+
+@bp.route("/")
+def welcome() -> str:
+    return "Welcome"
+
+
+
+
+
+
+
+
+
+
+def validateRequestSchema(request: dict, schema: Set[str]) -> bool:
+    for field in schema:
+        if field not in request:
+            return False
+        
+    for key in request:
+        if key not in schema:
+            return False
+        
+    return True
+
+
+
+if __name__ == "__main__":
+    app.register_blueprint(bp)
+
+    app.run(debug=True, port=port)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
