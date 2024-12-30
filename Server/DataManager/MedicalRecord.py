846083ff0f3123024657c923ec25e269a1b32fe7
kayatsa <kayatsa8@gmail.com>
2024-12-30 13:34:25 +0200

    Server/DataManager/MedicalRecord: same..

--- Server/DataManager/MedicalRecord.py
+++ Server/DataManager/MedicalRecord.py
@@ -8,10 +8,13 @@ class MedicalRecord(db.Model):
     field3: float = db.Column(db.Float, unique=False, nullable=False)
     field4: float = db.Column(db.Float, unique=False, nullable=False)
 
-    def __init__(self, name: int):
+    def __init__(self, field1: float, field2: float, field3: float, field4: float):
         super().__init__()
         # self.id = id
-        self.name = name
+        self.field1 = field1
+        self.field2 = field2
+        self.field3 = field3
+        self.field4 = field4
 
     def copy(self, other: 'MedicalRecord') -> None:
         for column in MedicalRecord.__table__.columns:
@@ -22,10 +25,10 @@ class MedicalRecord(db.Model):
     def toDict(self) -> dict:
         return {
             "id": self.id,
-            "field": self.field1,
-            "field": self.field1,
-            "field": self.field3,
-            "field": self.field4
+            "field1": self.field1,
+            "field2": self.field1,
+            "field3": self.field3,
+            "field4": self.field4
         }
     
 ATTRIBUTES: List[str] = ["id", "field1", "field2", "field3", "field4"]
