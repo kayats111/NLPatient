0a819e0e370d5cff468af7170118ad0afbf2a12e
kayatsa <kayatsa8@gmail.com>
2025-03-21 16:46:56 +0200

    Server/ModelTrainer/LearnModel: train method explaination and example

--- Server/ModelTrainer/LearnModel.py
+++ Server/ModelTrainer/LearnModel.py
@@ -2,10 +2,12 @@ from numpy.typing import NDArray
 from typing import List
 
 # This class will be run by the system, follow its guidelines.
+# Usage example with KNN is provided in comments
 class LearnModel:
 
     def __init__(self):
-        self.model = None  # put here a new model, for example: KNeighborsRegressor(n_neighbors=5) from Scikit-Learn
+        # put here a new model
+        self.model = None  # KNN example: self.model = KNeighborsRegressor(n_neighbors=5) from Scikit-Learn
 
         # only one of those should be true
         self.is_scikit: bool = False
@@ -14,7 +16,31 @@ class LearnModel:
         # use this field to add any metadata you would like, keep in mind that is_scikit and is_pytorch are saved automatically
         self.meta_data: dict = {}
 
-    
+        # add fields to your desire
+
+    def train(vectors: NDArray, labels: NDArray) -> None:
+        """
+        This method will be used to train (or partially train) your model according to
+        your preferences (train size, test size, batches, epochs) while using the system.
+        One run of this method is equivalent to one run on a batch of the training set.
+        If you do not have any batches or epochs, the model will be trained on all of
+        the data in one run, once.
+
+        Do not return anything from this method!
+        If you wish to save some data at the end of its run, please use the meta_data
+        field, or add fields as you desire.
+
+        Args:
+            vectors (NDArray[NDArray[float64]]): The Medical Records in vector form.
+            labels (NDArray[NDArray[float64]]): The labels of the Medical Records.
+
+        """
+
+        # KNN example:
+        # self.model.fit(vectors, labels)
+        # self.meta_data["last mse"] = mean_squared_error(labels, self.model.predict(vectors))
+
+
 
 
 
