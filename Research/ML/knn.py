99f56e2e8f1dc48f3c442a4ea36865b1581d6d92
kayatsa <kayatsa8@gmail.com>
2025-03-30 17:20:34 +0300

    Research/ML/knn: knn v2

--- Research/ML/knn.py
+++ Research/ML/knn.py
@@ -7,80 +7,22 @@ from sklearn.metrics import mean_squared_error, r2_score
 class LearnModel:
 
     def __init__(self, hyper_parameters: dict):
-        # put here a new model
-        self.model = KNeighborsRegressor(n_neighbors=5)
+        self.model = KNeighborsRegressor(hyper_parameters["k"])
         self.hyper_parameters: dict = hyper_parameters
 
-        # only one of those should be true
-        self.is_scikit: bool = False
-        self.is_pytorch: bool = False
-
-        # use this field to add any metadata you would like,
-        # keep in mind that is_scikit, is_pytorch and hyper_parameters are saved automatically
         self.meta_data: dict = {}
 
-        # add fields to your desire
-
     def train(self, vectors: NDArray, labels: NDArray) -> None:
-        """
-        This method will be used to train (or partially train) your model according to
-        your preferences (train size, test size, batches, epochs) while using the system.
-        One run of this method is equivalent to one run on a batch of the training set.
-        If you do not have any batches or epochs, the model will be trained on all of
-        the data in one run, once.
-
-        Do not return anything from this method!
-        If you wish to save some data at the end of its run, please use the meta_data
-        field, or add fields as you desire.
-
-        Args:
-            vectors (NDArray[NDArray[float64]]): The Medical Records in vector form.
-            labels (NDArray[NDArray[float64]]): The labels of the Medical Records.
-
-        NOTE: if you DO NOT wish to train your model, please keep it as it is. DO NOT delete it!
-
-        """
-
-        # KNN example:
-        # self.model.fit(vectors, labels)
-        # self.meta_data["last mse"] = mean_squared_error(labels, self.model.predict(vectors))
+        self.model.fit(vectors, labels)
 
     def test(self, vectors: NDArray, labels: NDArray) -> None:
-        """
-        This method will be used to test (or partially test) your model according to
-        your preferences (train size, test size, batches, epochs) while using the system.
-        One run of this method is equivalent to one run on a batch of the test set.
-        If you do not have any batches or epochs, the model will be tested on all of
-        the data in one run, once.
+        predicted = self.model.predict(vectors)
+        
+        mse = mean_squared_error(labels, predicted)
+        r2 = r2_score(labels, predicted)
 
-        Do not return anything from this method!
-        If you wish to save some data at the end of its run, please use the meta_data
-        field, or add fields as you desire.
-
-        NOTE: if you DO NOT wish to test your model, please keep it as it is. DO NOT delete it!
-
-        Args:
-            vectors (NDArray[NDArray[float64]]): The Medical Records in vector form.
-            labels (NDArray[NDArray[float64]]): The labels of the Medical Records.
-
-        """
-
-        # KNN example:
-        # predicted = self.model.predict(vectors)
-        # self.meta_data["last mse"] = mean_squared_error(labels, predicted)
-
-    """
-    You may add here any method you wish to help you with the model training and testing.
-
-    """
-
-
-
-"""
-You may add here any script, class or function to help you with the model training and testing.
-For example, you can add here a neural network definition.
-
-"""
+        self.meta_data["mse"] = mse
+        self.meta_data["r2"] = r2
 
 
 
