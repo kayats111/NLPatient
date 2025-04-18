9fcb8706ece4b1ec21f3e210cf200d86ce438ef0
kayatsa <kayatsa8@gmail.com>
2025-04-18 16:19:18 +0300

    Research/DL/fc_2Layers: train and test process of fc_2Layers.

--- Research/DL/fc_2Layers.py
+++ Research/DL/fc_2Layers.py
@@ -7,15 +7,40 @@ import torch.nn as nn
 class LearnModel:
 
     def __init__(self, hyper_parameters: dict):
-        self.model = None
+        self.model = fc(hyper_parameters=hyper_parameters)
         self.hyper_parameters: dict = hyper_parameters
         self.meta_data: dict = {}
 
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.hyper_parameters["learning_rate"])
+        self.meta_data["loss"] = 0
+        self.meta_data["accuracy"] = 0
+
+        self.correct = 0
+        self.total = 0
+
     def train(self, vectors: NDArray, labels: NDArray) -> None:
-        pass
+        outputs = self.model(vectors)
+        
+        loss = self.criterion(outputs, labels)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
 
     def test(self, vectors: NDArray, labels: NDArray) -> None:
-        pass
+        with torch.no_grad():
+            outputs = self.model(vectors)
+
+            self.meta_data["loss"] = self.criterion(outputs, labels).item()
+
+            _, predicted = torch.max(outputs.data, 1)
+            self.total += labels.size(0)
+            self.correct += (predicted == labels).sum().item()
+
+            self.meta_data["accuracy"] = 100 * self.correct / self.total
+
+
 
 
 
