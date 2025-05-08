16740cf9df3b70e0f0de19dfbe590a4ccc9da4ac
kayatsa <kayatsa8@gmail.com>
2025-05-08 10:18:04 +0300

    Server/DataManager/TextService: constructor

--- Server/DataManager/TextService.py
+++ Server/DataManager/TextService.py
@@ -1,6 +1,11 @@
 from typing import Dict, List, Set
-from Repository import Repository
-from MedicalRecord import ATTRIBUTES, LABELS, TRAIN_ATTRIBUTES, MedicalRecord
+from TextRepository import TextRepository
+from MedicalRecordText import ATTRIBUTES, LABELS, TRAIN_ATTRIBUTES, MedicalRecordText
+
+class TextService:
+
+    def __init__(self):
+        self.repository: TextRepository = TextRepository()
 
 
 
