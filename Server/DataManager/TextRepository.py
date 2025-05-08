1703177f48c54e28b871b2f583731c0f61e11c68
kayatsa <kayatsa8@gmail.com>
2025-05-08 09:43:51 +0300

    Server/DataManager/TextRepository: delete_record() + update_record()

--- Server/DataManager/TextRepository.py
+++ Server/DataManager/TextRepository.py
@@ -23,9 +23,28 @@ class TextRepository:
         
         return record
         
+    def delete_record(self, id: int) -> None:
+        record: MedicalRecordText = self.get_record_by_id(id)
 
+        if record is None:
+            raise Exception("no such record")
+        
+        try:
+            db.session.delete(record)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            raise Exception("cannot delete record")
 
+    def update_record(self, record: MedicalRecordText) -> None:
+        existing: MedicalRecordText = self.get_record_by_id(record.id)
 
+        if record is None:
+            raise Exception("no such record")
+
+        existing.copy(record)
+
+        db.session.commit()   
 
 
 
