6c19c7f56262e5471a72767c3626a61403897969
kayatsa <kayatsa8@gmail.com>
2025-05-08 10:20:43 +0300

    Server/DataManager/TextService: add_record()

--- Server/DataManager/TextService.py
+++ Server/DataManager/TextService.py
@@ -7,8 +7,11 @@ class TextService:
     def __init__(self):
         self.repository: TextRepository = TextRepository()
 
+    def add_record(self, record_dict: dict) -> None:
+        record: MedicalRecordText = self.record_from_dict(record_dict=record_dict)
+        self.repository.add_record(record)
 
-
+    
 
 
 
