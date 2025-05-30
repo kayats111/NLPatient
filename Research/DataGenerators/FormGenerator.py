4c5c4dff396d9222cc41df6fb08c86baaa631a2a
kayatsa <kayatsa8@gmail.com>
2025-05-30 18:18:32 +0300

    Research/DataGenerators/FormGenerator: basic script to read records and print them

---
+++ Research/DataGenerators/FormGenerator.py
@@ -0,0 +1,53 @@
+import sqlite3
+import requests
+from Response import Response
+
+
+fields = ['codingNum', 'yearOfEvent', 'age',
+          'gender', 'sector', 'origin', 'originGroup',
+          'immigrationYear', 'LMSSocialStateScore',
+          'clalitMember', 'parentState', 'parentStateGroup',
+          'livingWith', 'livingWithGroup', 'siblingsTotal',
+          'numInSiblings', 'familyHistoryMH', 'school',
+          'schoolGroup', 'prodrom', 'prodGroup', 'posLengthGroup',
+          'psLengthG2', 'vocalHallucinations', 'visualHallucinations',
+          'dellusions', 'disorgenizeBahaviour', 'thoughtProcess',
+          'speechSym', 'negSigns', 'sleepDisorder', 'catatonia',
+          'maniformSym', 'depressizeSym', 'drugUseCurrent',
+          'drugUseHistory', 'traditionalTreat', 'violence',
+          'irritabilityAnamneza', 'suicidal', 'backdiagnos',
+          'organicworkup', 'conhospi', 'noconhospi', 'futurediag',
+          'any', 'affective', 'bipolar', 'schizophreniaSpectr',
+          'psychotic2nd']
+
+
+records = []
+
+
+
+try:
+    with sqlite3.connect('PsychosisProject.db') as conn:
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM PsychosisTable')
+
+        rows = cur.fetchall()
+
+        records = [{field: value for field, value in zip(fields, row)} for row in rows]
+
+except sqlite3.OperationalError as e:
+    print(e)
+
+
+for record in records:
+    record.pop("backdiagnos")
+    record.pop("noconhospi")
+    record.pop("futurediag")
+    record.pop("psychotic2nd")
+    record.pop("any")
+    record.pop("affective")
+    record.pop("bipolar")
+    record.pop("schizophreniaSpectr")
+
+    print("-------------------------------------------------")
+    print(record)
+    print("-------------------------------------------------")
